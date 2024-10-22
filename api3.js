0e3fc00deb29e08013773a777bf87f306b3f2803
SRINIVAS2002 <107999059+SRINIVAS2002@users.noreply.github.com>
2024-10-22 10:08:28 +0530

    Update api3.js

--- api3.js
+++ api3.js
@@ -3,7 +3,7 @@ fetch(link).then(res=>{
     if(res.ok) return res.json()
     else {
     console.log("Error:",res.status)
-    }))
+    })
 .then(fet=>{
     document.write(`<h1 style="background-color: orange; padding : 10px; margin :50px; text-align: center;">ID - ALBUM ID => TITLE NAME</h1>`)
     for(i of fet)
