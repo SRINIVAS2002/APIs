d22e661a9f64897c089e0e4c3bc4400061736b64
SRINIVAS2002 <107999059+SRINIVAS2002@users.noreply.github.com>
2024-10-22 08:47:02 +0530

    Update api3.js

--- api3.js
+++ api3.js
@@ -1,5 +1,9 @@
 const link="https://jsonplaceholder.typicode.com/photos"
-fetch(link).then(r=>r.json())
+fetch(link).then(res=>{
+    if(res.ok) return res.json()
+    else {
+    console.log("Error:",res.status)
+    }))
 .then(fet=>{
     document.write(`<h1 style="background-color: orange; padding : 10px; margin :50px; text-align: center;">ID - ALBUM ID => TITLE NAME</h1>`)
     for(i of fet)
