748f6e147b000085ba0073addd848088e09650b7
SRINIVAS2002 <107999059+SRINIVAS2002@users.noreply.github.com>
2024-10-22 10:15:31 +0530

    Update api2.js

--- api2.js
+++ api2.js
@@ -3,7 +3,8 @@ fetch(link).then(res=>{
     if(res.ok) return res.json()
     else {
     console.log("Error:",res.status)
-    })
+    }
+})
 .then(fet=>{
     document.write(`<h1 style="background-color: orange; padding : 10px; margin :50px; text-align: center;">USER ID => WORK TO DO = WORK STATUS</h1>`)
     for(i of fet["todos"])
