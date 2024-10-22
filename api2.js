2de6352f5f042ad6d15283740ec0949593ed357c
SRINIVAS2002 <107999059+SRINIVAS2002@users.noreply.github.com>
2024-10-22 08:46:45 +0530

    Update api2.js

--- api2.js
+++ api2.js
@@ -1,5 +1,9 @@
 const link="https://dummyjson.com/todos"
-fetch(link).then(r=>r.json())
+fetch(link).then(res=>{
+    if(res.ok) return res.json()
+    else {
+    console.log("Error:",res.status)
+    }))
 .then(fet=>{
     document.write(`<h1 style="background-color: orange; padding : 10px; margin :50px; text-align: center;">USER ID => WORK TO DO = WORK STATUS</h1>`)
     for(i of fet["todos"])
